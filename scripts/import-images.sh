37421074ac86c32b59d67fdc1d6766c09aab41ac
Nianyu Shen <xiaoyu9964@gmail.com>
2025-09-10 18:11:35 -0700

    fix: make import.sh POSIX compatiable

--- scripts/import-images.sh
+++ scripts/import-images.sh
@@ -6,4 +6,6 @@ CONTENT_PATH=$1
 
 mkdir -p /var/snap/k8s/common/images
 
-find "$CONTENT_PATH" -name "*.tar" -type f -exec cp {} /var/snap/k8s/common/images \;
+find -L "$CONTENT_PATH" -name "*.tar" -type f | while read -r tarfile; do
+    cp "$tarfile" /var/snap/k8s/common/images/
+done
