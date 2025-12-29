2fb9bda6fdead288612e3d0466c9eb2468c07bce
Arun Sharma <arun.sharma@spectrocloud.com>
2025-12-29 23:02:33 +0530

    PE-7896: MNC cluster fix broken upgrade (#32)
    * PE-7896: MNC cluster fix broken upgrade
    
    * refactor to install with retry
    
    Signed-off-by: Dr-N00B <dev.arun2302@gmail.com>
    
    * add logging
    
    Signed-off-by: Dr-N00B <dev.arun2302@gmail.com>
    
    * don't exit on error
    
    Signed-off-by: Dr-N00B <dev.arun2302@gmail.com>
    
    ---------
    
    Signed-off-by: Dr-N00B <dev.arun2302@gmail.com>

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -1,74 +1,66 @@
 #!/bin/bash
 
-set -x
-
-trap 'echo -n $(date)' DEBUG
-
-exec   > >(tee -ia /var/log/canonical-upgrade.log)
-exec  2> >(tee -ia /var/log/canonical-upgrade.log >& 2)
-
-node_role=$1
-current_node_name=$(cat /etc/hostname)
-
-current_installed_revision=$(snap list k8s | grep k8s | awk '{print $3}')
-echo "current k8s revision: $current_installed_revision"
-
-upcoming_revision=$(cat /opt/canonical/k8s.revision)
-if [ "$current_installed_revision" = "$upcoming_revision" ]; then
-    echo "k8s is already up to date"
-    exit 0
-fi
+source "$(dirname "$0")/common.sh"
+setup_logging /var/log/canonical-upgrade.log
+set -u
 
 export KUBECONFIG=/etc/kubernetes/admin.conf
+current_node_name=$(cat /etc/hostname)
+
+# -------- inputs --------
+node_role=$1
+
+log "starting canonical k8s upgrade"
+
+current_installed_revision=$(snap list k8s | grep k8s | awk '{print $3}')
+
+upcoming_revision=$(cat /opt/canonical/k8s.revision)
+log "current installed k8s revision: $current_installed_revision"
+log "upcoming k8s revision to install: $upcoming_revision"
+if [ "$current_installed_revision" = "$upcoming_revision" ]; then
+	log "k8s is already up to date"
+	exit 0
+fi
+
+log "upgrading k8s from $current_installed_revision to $upcoming_revision"
 
 get_current_upgrading_node_name() {
-  kubectl get configmap upgrade-lock -n kube-system -o jsonpath="{['data']['node']}"
+	kubectl get configmap upgrade-lock -n kube-system -o jsonpath="{['data']['node']}"
 }
 
 delete_lock_config_map() {
-  # Delete the configmap lock once the upgrade completes
-  if [ "$node_role" != "worker" ]
-  then
-    kubectl delete configmap upgrade-lock -n kube-system
-  fi
+	# Delete the configmap lock once the upgrade completes
+	if [ "$node_role" != "worker" ]; then
+		kubectl delete configmap upgrade-lock -n kube-system
+	fi
+}
+
+acquire_lock() {
+	if [ "$node_role" != "worker" ]; then
+		until kubectl create configmap upgrade-lock -n kube-system --from-literal=node="${current_node_name}" > /dev/null; do
+			upgrade_node=$(get_current_upgrading_node_name)
+			if [ "$upgrade_node" = "$current_node_name" ]; then
+				log "resuming upgrade"
+				break
+			fi
+			log "failed to create configmap for upgrade lock, upgrade in progress on node ${upgrade_node}; retrying in 60s..."
+			sleep 60
+		done
+	fi
 }
 
 do_upgrade() {
-    if [ "$node_role" != "worker" ]; then
-      until kubectl create configmap upgrade-lock -n kube-system --from-literal=node="${current_node_name}" > /dev/null
-      do
-        upgrade_node=$(get_current_upgrading_node_name)
-        if [ "$upgrade_node" = "$current_node_name" ]; then
-          echo "resuming upgrade"
-          break
-        fi
-        echo "failed to create configmap for upgrade lock, upgrading is going on the node ${upgrade_node}, retrying in 60 sec"
-        sleep 60
-      done
-    fi
+	acquire_lock
 
-    snap wait system seed.loaded
+	install_all_snaps
 
-    snapd_revision=$(cat /opt/canonical/snapd.revision)
-    core_revision=$(cat /opt/canonical/core.revision)
-    k8s_revision=$(cat /opt/canonical/k8s.revision)
+	if [ "$node_role" != "worker" ]; then
+		wait_for_k8s_ready
+	fi
 
-    cd /opt/canonical-k8s
-    snap ack snapd_"${snapd_revision}".assert && sudo snap install ./snapd_"${snapd_revision}".snap
-    snap ack core*_"${core_revision}".assert && sudo snap install ./core*_"${core_revision}".snap --classic
-    snap ack k8s_"${k8s_revision}".assert && sudo snap install ./k8s_"${k8s_revision}".snap --classic
+	hold_k8s_snap_refresh
 
-    if [ "$node_role" != "worker" ]; then
-        until k8s status --wait-ready
-        do
-            echo "waiting for status"
-            sleep 10
-        done
-    fi
-
-    snap refresh k8s --hold
-
-    delete_lock_config_map
+	delete_lock_config_map
 }
 
 do_upgrade
