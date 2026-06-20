221eebc962a410348f48bd15b1888697439f7ac1
Arun Sharma <arun.sharma@spectrocloud.com>
2026-06-21 01:34:43 +0530

    sign off
    Signed-off-by: Arun Sharma <arun.sharma@spectrocloud.com>

--- scripts/pre-setup.sh
+++ scripts/pre-setup.sh
@@ -6,7 +6,7 @@ load_provider_environment
 
 # legacy vs systemd-extension compatibility
 # Check if /usr/lib/containerd/config.toml exists. If it exists, copy it to /etc/containerd/config.toml.
-# Extensions store config.toml in /usr/lib/containerd.
+# Canonical systemd extensions store config.toml in /usr/lib/containerd.
 if [[ -f /usr/lib/containerd/config.toml ]]; then
     mkdir -p /etc/containerd
     cp -f /usr/lib/containerd/config.toml /etc/containerd/config.toml
