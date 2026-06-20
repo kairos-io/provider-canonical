8d45094ff2cb34a434f5c685aa3f00c222109b8a
Arun Sharma <arun.sharma@spectrocloud.com>
2026-06-21 01:32:50 +0530

    fix: canonical upgrade with systemd extensions

--- scripts/pre-setup.sh
+++ scripts/pre-setup.sh
@@ -4,5 +4,13 @@ set -x
 
 load_provider_environment
 
+# legacy vs systemd-extension compatibility
+# Check if /usr/lib/containerd/config.toml exists. If it exists, copy it to /etc/containerd/config.toml.
+# Extensions store config.toml in /usr/lib/containerd.
+if [[ -f /usr/lib/containerd/config.toml ]]; then
+    mkdir -p /etc/containerd
+    cp -f /usr/lib/containerd/config.toml /etc/containerd/config.toml
+fi
+
 systemctl enable snapd
 systemctl restart snapd
